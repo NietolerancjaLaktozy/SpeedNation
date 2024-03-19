5d9ef95ec06b878a7b988b1a681f9e9058086d39
NietolerancjaLaktozy <160734507+NietolerancjaLaktozy@users.noreply.github.com>
2024-03-19 12:47:32 +0100

    poczatek

---
+++ database/migrations/2024_03_05_130502_create_car_rentals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_rentals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_car');
+            $table->integer('id_user');
+            $table->date('start_date');
+            $table->date('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_rentals');
+    }
+};
---
+++ resources/views/kontakt.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+
+    <div class="c-blue content">
+        <h1>Kontakt</h1>
+            Numer telefonu: 515364456
+            E-mail Kontakt@SpeedNation.pl
+    </div>
+    
+</x-app-layout>
