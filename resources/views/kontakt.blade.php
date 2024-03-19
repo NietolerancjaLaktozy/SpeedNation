b37cc599f03f9ca98dda7d5e5d161216536dea40
NietolerancjaLaktozy <160734507+NietolerancjaLaktozy@users.noreply.github.com>
2024-03-19 12:56:13 +0100

    d

--- resources/views/kontakt.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-
-    <div class="c-blue content">
-        <h1>Kontakt</h1>
-            Numer telefonu: 515364456
-            E-mail Kontakt@SpeedNation.pl
-    </div>
-    
-</x-app-layout>
